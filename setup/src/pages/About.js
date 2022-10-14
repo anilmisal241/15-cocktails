64dc1cf6dee47655f5222e1c9e9a6ab654092bff
Anil Misal <anilmisal241@gmail.com>
2022-10-14 15:59:29 +0530

    initial commit

---
+++ setup/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-ection">
+      <h1 className="section-title">about Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem harum
+        deserunt magnam, cum unde autem sunt voluptas molestias omnis eum in sit
+        obcaecati sequi animi vitae similique saepe quia dolores nesciunt!
+        Asperiores laborum earum magni sapiente reiciendis voluptatibus
+        blanditiis atque itaque doloremque nemo. Dicta, perferendis?
+      </p>
+    </section>
+  );
+};
+
+export default About;
